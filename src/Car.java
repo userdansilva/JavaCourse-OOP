d6ebf3a3e9b611121195b41179e5ffca2f007625
Daniel Silva <daniel.sousasilva97@outlook.com>
2023-08-24 19:30:48 -0300

    OOP module has been restarted

--- src/Car.java
+++ src/Car.java
@@ -1,20 +1,24 @@
 public class Car {
     private String make;
     private String model;
+    private String color;
+    private int doors;
+    private boolean convertible;
 
-    public Car(String make, String model) {
-        this.make = make;
-        this.model = model;
+    public void describeCar() {
+        System.out.println("The car " + model +
+                " made by " + make +
+                " with the color " + color +
+                " has " + doors +
+                " doors and it is " + (convertible ? "convertible" : "not convertible"));
     }
 
-    public Car(String model) {
+    public Car(String make, String model, String color, int doors, boolean convertible) {
+        this.make = make;
         this.model = model;
-
-        switch (model) {
-            case "Palio", "Argo" -> this.make = "Fiat";
-            case "Celta" -> this.make = "Chevrolet";
-            default -> this.make = "Unknown";
-        }
+        this.color = color;
+        this.doors = doors;
+        this.convertible = convertible;
     }
 
     public String getMake() {
@@ -25,11 +29,35 @@ public class Car {
         return model;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
     public void setMake(String make) {
         this.make = make;
     }
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String textBlock = """
-                    List items
-                        • First Item
-                        • Second Item""";
+        Car car = new Car("Fiat", "Palio", "red", 4, false);
+        car.describeCar();
 
-        System.out.println(textBlock);
-
-        System.out.printf("My age is %d and my birth year is %d, and a I have %.2f", 25, 1997, (float) 10.0);
-
-        String formattedString = String.format("I have R$ %.2f", 35.50);
-        System.out.println(formattedString);
-
-        String otherFormatted = "I have R$ %.2f".formatted(35.50);
-        System.out.println(otherFormatted);
+        System.out.println(car.getMake());
     }
 }
