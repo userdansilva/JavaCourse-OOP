33159884e0fef16d2240c4c992b4007148d3cc08
Daniel Silva <daniel.sousasilva97@outlook.com>
2023-08-29 06:51:59 -0300

    learning record

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Customer daniel = new Customer();
-        System.out.println(daniel.toString());
+        for( int i = 1; i<= 5; i++) {
+            String name;
 
-        Customer carlos = new Customer("Carlos", "carlos@mail.com");
-        System.out.println(carlos.toString());
+            switch (i) {
+               case 1 -> name = "Daniel";
+               case 2 -> name = "Pedro";
+               case 3 -> name = "Augusto";
+               case 4 -> name = "Ana";
+               case 5 -> name = "Julia";
+               default -> name = "Unsupported";
+            }
 
-        Customer ana = new Customer("Ana", 2_000.0, "ana@mail.com");
-        System.out.println(ana);
+            Student student = new Student(i, name, "13/10/1997", List.of("Reactjs", "Vuejs"));
+            System.out.println(student.name());
+            System.out.println(student.toString());
+        }
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,4 @@
+import java.util.List;
+
+public record Student(int id, String name, String dateOfBirth, List<String>classList) {
+}
